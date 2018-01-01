e39ee2e9c54c8a64db846b712b9bac9b3348d8a1
mansiMK <mansikachchhi171@gmail.com>
2018-01-01 16:09:30 +0530

    commit 11

--- application/controllers/Artist.php
+++ application/controllers/Artist.php
@@ -9,13 +9,19 @@ class Artist extends MY_Controller {
         $this->load->model('country_model');
         $this->load->model('state_model');
         $this->load->model('city_model');
+        $this->load->model('user_model');
+    $this->load->model('artist_model');
+    
+        
     }
 
     public function index() {
         $data['country_list'] = $this->country_model->getcountrylist();
         $data['state_list'] = $this->state_model->getstatelist();
         $data['city_list'] = $this->city_model->getcitylist();
-        $data['artist_list'] = $this->artist_model->getuserlist();
+     //   $data['user_list'] = $this->user_model->getuserlist();
+        $data['artist_list'] = $this->artist_model->getartistlist();
+        
         $this->load->view('v_artist_view', $data);
     }
 
@@ -23,55 +29,60 @@ class Artist extends MY_Controller {
         $data['country_list'] = $this->country_model->getcountrylist();
         $data['state_list'] = $this->state_model->getstatelist();
         $data['city_list'] = $this->city_model->getcitylist();
-        $data['artist_list'] = $this->artist_model->getuserlist();
+     //   $data['user_list'] = $this->user_model->getuserlist();
+        $data['artist_list'] = $this->artist_model->getartistlist();
         $this->load->view('v_artist_view', $data);
     }
 
-    public function add_user() {
+    public function add_artist() {
         $data['country_list'] = $this->country_model->getcountrylist();
         $data['state_list'] = $this->state_model->getstatelist();
         $data['city_list'] = $this->city_model->getcitylist();
-        $data['artist_list'] = $this->artist_model->getuserlist();
+     //   $data['user_list'] = $this->user_model->getuserlist();
+       $data['artist_list'] = $this->artist_model->getartistlist();
+        
         $this->load->view('v_artist_form', $data);
     }
 
         public function addp() {
-        $user_data = $this->artist_model->check_data($_POST['first_name'],$_POST['last_name'],$_POST['country_id'],$_POST['state_id'],$_POST['city_id'],$_POST['pincode'],$_POST['email'],$_POST['mobile']);
-        if (isset($user_data)) {
+        $artist_data = $this->artist_model->check_data($_POST['first_name'],$_POST['last_name'],$_POST['art_category_id'],$_POST['mobile'],$_POST['email'],$_POST['password'],$_POST['country_id'],$_POST['state_id'],$_POST['city_id'],$_POST['pincode']);
+        if (isset($artist_data)) {
             $this->session->set_flashdata('message','record already exists...');            
-            redirect('city/index');
+            redirect('artist/index');
         } else {
-            $this->artist_model->insert($_POST['first_name'],$_POST['last_name'],$_POST['country_id'],$_POST['state_id'],$_POST['city_id'],$_POST['pincode'],$_POST['email'],$_POST['mobile']);
+            $this->artist_model->insert($_POST['first_name'],$_POST['last_name'],$_POST['art_category_id'],$_POST['mobile'],$_POST['email'],$_POST['password'],$_POST['country_id'],$_POST['state_id'],$_POST['city_id'],$_POST['pincode']);
+            
              $this->session->set_flashdata('message','insert successfully...');
-            redirect('city/index');
+            redirect('artist/index');
         }
     }
     
      public function drop_state() {
-        $data['update_data'] = $this->user_model->drop_state($_POST['country_id']);
+        $data['update_data'] = $this->artist_model->drop_state($_POST['country_id']);
         $this->load->view('drop_state', $data);
         //     print_r( $data['update_data']);
     }
     
     public function drop_city() {    
-        $data['update_data'] = $this->user_model->drop_city($_POST['state_id']);
+        $data['update_data'] = $this->artist_model->drop_city($_POST['state_id']);
         $this->load->view('drop_city', $data);
     }
 
-    public function edit_data($user_id) {
-        $data['update_data'] = $this->user_model->edit_data($user_id);
+    public function edit_data($artist_id) {
+        $data['update_data'] = $this->artist_model->edit_data($artist_id);
 
         $data['country_list'] = $this->country_model->getcountrylist();
         $data['state_list'] = $this->state_model->getstatelist();
         $data['city_list'] = $this->city_model->getcitylist();
-$data['user_list'] = $this->user_model->getuserlist();
+$data['artist_list'] = $this->artist_model->getartistlist();
 
-        $this->load->view('v_user_form', $data);
+        $this->load->view('v_artist_form', $data);
     }
 
     public function editp() {
-        $this->user_model->update_data($_POST['user_id'], $_POST['first_name'],$_POST['last_name'],$_POST['country_id'],$_POST['state_id'],$_POST['city_id'],$_POST['pincode'],$_POST['email'],$_POST['mobile']);
-        redirect("user/index");
+        $this->artist_model->update_data($_POST['artist_id'],$_POST['first_name'],$_POST['last_name'],$_POST['art_category_id'],$_POST['mobile'],$_POST['email'],$_POST['password'],$_POST['country_id'],$_POST['state_id'],$_POST['city_id'],$_POST['pincode']);
+            
+        redirect("artist/index");
     }
 
     /*public function update_data($user_id) {
@@ -85,30 +96,30 @@ $data['user_list'] = $this->user_model->getuserlist();
         $this->load->view('v_city_view', $data);
     }*/
     public function update_data($user_id) {
-        $data['user_list'] = $this->user_model->getuserlist();
-        $data['country_list'] = $this->country_model->getcountrylist();
+      $data['artist_list'] = $this->artist_model->getartistlist();
+          $data['country_list'] = $this->country_model->getcountrylist();
         $data['update_data'] = $this->state_model->getstatelist();
         $data['update_data'] = $this->city_model->getcitylist();
-        $data['user_data'] = $this->user_model->edit_data($user_id);
-        $data['user_list'] = $this->user_model->getuserlist();
-        $this->load->view('v_user_view', $data);
+        $data['artist_data'] = $this->artist_model->edit_data($artist_id);
+        $data['artist_list'] = $this->artist_model->getartistlist();
+        $this->load->view('v_artist_view', $data);
     }
 
 
-  public function delete($user_id) {
-        $this->user_model->delete($user_id);
-        redirect("user/index");
+  public function delete($artist_id) {
+        $this->artist_model->delete($artist_id);
+        redirect("artist/index");
     }
     
-    public function update_status_active($user_id) {
+    public function update_status_active($artist_id) {
         $status = $this->input->get('status');
-        $this->user_model->update_active($user_id, $status);
-        redirect('user/index');
+        $this->artist_model->update_active($artist_id, $status);
+        redirect('artist/index');
     }
 
-    public function update_status_deactive($user_id) {
+    public function update_status_deactive($artist_id) {
         $status = $this->input->get('status');
-        $this->user_model->update_deactive($user_id, $status);
-        redirect('user/index');
+        $this->artist_model->update_deactive($artist_id, $status);
+        redirect('artist/index');
     }
 }
---
+++ application/models/Artist_model.php
@@ -0,0 +1,100 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Artist_model extends CI_model {
+
+    public function getartistlist() {
+        $query = $this->db->query("select * from artist_master");
+        return $query->result();
+    }
+
+    public function insert($first_name, $last_name, $art_category_id, $mobile, $email, $password, $country_id, $state_id, $city_id, $pincode) {
+        $data = array('first_name' => $first_name,
+            'last_name' => $last_name,
+            'art_category_id' => $art_category_id,
+            'mobile' => $mobile,
+            'email' => $email,
+            'passord' => $password,
+            'country_id' => $country_id,
+            'state_id' => $state_id,
+            'city_id' => $city_id,
+            'pincode' => $pincode,
+        );
+        $this->db->insert('artist_master', $data);
+    }
+
+    //chk if record exists in database  or not 
+    public function check_data($first_name, $last_name, $art_category_id, $mobile, $email, $password, $country_id, $state_id, $city_id, $pincode) {
+        $query = $this->db->query("select * from artist_master where first_name='$first_name' AND  last_name='$last_name' AND
+                                                      mobile='$mobile'    AND
+                                                          email='$email'  AND
+                                                             password='$password'   AND
+                                                                country_id='$country_id' AND 
+                                                                 state_id='$state_id' AND
+                                                                      city_id='$city_id' AND
+                                                                          pincode = '$pincode' 
+                                                                             
+                                                                                   ");
+        return $query->row_array();
+    }
+
+    public function drop_state($country_id) {
+        $query = $this->db->query("select * from  state_master where country_id='$country_id' ");
+        return $query->result();
+    }
+
+    public function drop_city($state_id) {
+        $query = $this->db->query("select * from  city_master where state_id='$state_id' ");
+        return $query->result();
+    }
+
+    public function edit_data($artist_id) {
+        $query = $this->db->query("select * from  artist_master  where artist_id='$artist_id' ");
+        return $query->row_array();
+    }
+
+    public function update_data($user_id, $first_name, $last_name, $art_category_id, $mobile, $email, $password, $country_id, $state_id, $city_id, $pincode) {
+        
+         $data = array(
+              'artist_id' => $artist_id,
+           
+             'first_name' => $first_name,
+            'last_name' => $last_name,
+            'art_category_id' => $art_category_id,
+            'mobile' => $mobile,
+            'email' => $email,
+            'passord' => $password,
+            'country_id' => $country_id,
+            'state_id' => $state_id,
+            'city_id' => $city_id,
+            'pincode' => $pincode,
+        );
+        $this->db->where('artist_id', $artist_id);
+        $this->db->update('artist_master', $data);
+    }
+
+    public function delete($artist_id) {
+        $this->db->where('artist_id', $artist_id);
+        $this->db->delete('artist_master');
+    }
+
+    public function update_active($artist_id, $status) {
+        $data = array(
+            'user_id' => $artist_id,
+            'status' => 1
+        );
+        $this->db->where('artist_id', $artist_id);
+        $this->db->update('artist_master', $data);
+    }
+
+    public function update_deactive($artist_id, $status) {
+        $data = array(
+            'artist_id' => $artist_id,
+            'status' => 0
+        );
+        $this->db->where('artist_id', $artist_id);
+        $this->db->update('artist_master', $data);
+    }
+
+}
