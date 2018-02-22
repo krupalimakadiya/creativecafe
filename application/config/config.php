87debb848c120b78f0ec3ae4e4003b0538ebb3a0
php <php@gmial.com>
2018-02-22 23:01:10 +0530

    config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,17 +25,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/creativecafe/';
 $config['static_url'] = 'http://localhost/AdminLTE/';
-<<<<<<< HEAD
 $config['static_url1'] ='C:/wamp/www/event/';
 $config['static_url2'] ='http://localhost/event/';
-=======
 $config['image_url']='C:\wamp\www\artist_image';
 //$config['image_path']='http://localhost/artist_image/';
 $config['post_url']='http://localhost/post/';
 $config['post_path']='C:\wamp\www\post';
 
-
->>>>>>> 1071d8185190d31b30d65dd0504f4599c893e5c4
 
 /*
 |--------------------------------------------------------------------------
