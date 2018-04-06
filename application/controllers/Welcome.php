8117098a0e44ec3d5c9ea9424d2e83544e146654
krupalimakadiya <krupalimakadiya123@gmail.com>
2018-04-06 19:05:22 +0530

    ejwrktj

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,11 +1,15 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends MY_Controller {
 
-	
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function index() {
+        $this->load->model('welcome_model');
+        $data['post'] = $this->welcome_model->get_total_post();
+        $data['artist'] = $this->welcome_model->get_total_artist();
+        $data['user'] = $this->welcome_model->get_total_user();
+        $this->load->view('welcome_message',$data);
+    }
+
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -26,41 +26,87 @@ $this->load->view('admin/header_body_aside');
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Blank page
-        <small>it all starts here</small>
+        Dashboard
+        <small>Control panel</small>
       </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li>
-      </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-         
+              <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
         </div>
-        <div class="box-body">
-          Start creating your amazing application!
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3><?php echo $artist->total; ?></h3>
+
+              <p>Artist Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer"><i class="fa fa-arrow-circle-right"></i></a>
+          </div>
         </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>54</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
         </div>
-        <!-- /.box-footer-->
+        <!-- ./col -->
       </div>
-      <!-- /.box -->
+      <!-- /.row -->
+      <!-- Main row -->
+      <div class="row">
+        <!-- right col -->
+      </div>
+      <!-- /.row (main row) -->
 
     </section>
     <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
+    <!-- /.content-wrapper -->
 
   <footer class="main-footer">
   <?php
